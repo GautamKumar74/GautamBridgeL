851e76b2513b49a38c1073bfe25a421068eb9ed1
Gautam Kumar <gk62018@gmail.com>
2025-01-07 11:54:05 +0530

    Multiplication functionality added

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Multiplication{
+   public static void main(String[] args){
+      System.out.println("Enter two integers to multiply");
+      Scanner sc= new Scanner(System.in);
+      int a= sc.nextInt();
+      int b= sc.nextInt();
+      System.out.println("Multiplication : " + (a*b)); 
+   }
+}
