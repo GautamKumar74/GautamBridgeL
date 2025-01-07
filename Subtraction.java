f22325ed9c87b76282483eaf09182071fd3c6247
Gautam Kumar <gk62018@gmail.com>
2025-01-07 11:13:33 +0530

    Subtraction functionality added

---
+++ Subtraction.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Subtraction{
+   public static void main(String[] args){
+      System.out.println("Enter two integers to subtract");
+      Scanner sc= new Scanner(System.in);
+      int a= sc.nextInt();
+      int b= sc.nextInt();
+      System.out.println("Subtraction : " + (a-b)); 
+   }
+}
